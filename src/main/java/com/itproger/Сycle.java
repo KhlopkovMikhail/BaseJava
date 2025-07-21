9f2a549258c5cfea1beae2cabd46bdbe51c097da
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-07-21 22:57:34 +0300

    add Cycle

---
+++ src/main/java/com/itproger/Сycle.java
@@ -0,0 +1,26 @@
+// Циклы
+package com.itproger;
+
+public class Сycle {
+
+    public static void main(String[] args) {
+
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println("Element: " + i);
+//        }
+//        for (float i = 100; i > 10; i /= 2) {
+//            System.out.println("Element: " + i);
+//        }
+//        int i = 0;
+//        while (i <= 10) {
+//            System.out.println("Element " + i);
+//            i++;
+//        }
+
+        int i = 0;
+        do {
+            System.out.println("Element " + i);
+            i++;
+        } while (i < 10);
+    }
+}
