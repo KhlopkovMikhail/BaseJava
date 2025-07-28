ad8e13566d9c98049c15f7c45f9b93ef7e93bd0d
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-07-28 21:58:22 +0300

    add comment in Array

--- src/main/java/com/itproger/Array.java
+++ src/main/java/com/itproger/Array.java
@@ -17,7 +17,7 @@ public class Array {
         float[] nums2 = new float[] {5.0f, 6.45f, 89.984f};
         System.out.println(nums2[1]);
 
-        for(int i = 0; i < nums2.length; i++) {
+        for(int i = 0; i < nums2.length; i++) {  // nums2.lenght - указывается переберать до крайнего элемента в массиве
             System.out.println("Element: " + nums2[i]);
         }
     }
