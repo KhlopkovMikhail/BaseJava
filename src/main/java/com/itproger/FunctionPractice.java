ec5087b15cb6f6ee8ff073fd939aa5d64bc6491c
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-08-04 21:18:18 +0300

    add FunctionsPractice

---
+++ src/main/java/com/itproger/FunctionPractice.java
@@ -0,0 +1,27 @@
+// Методы в Java
+package com.itproger;
+
+public class FunctionPractice {
+
+    public static void main(String[] args) {
+
+        byte[] nums1 = new byte[]{5, 6, 8};
+
+        int summ1 = summaArray(nums1);
+        System.out.println("Сумма 1: " + summ1);
+
+        byte[] nums2 = new byte[]{5, 7, 3, 7, 6, 8};
+
+        int summ2 = summaArray(nums1);
+        System.out.println("Сумма 2: " + summ2);
+    }
+
+    public static int summaArray(byte[] arr) {
+        int summa = 0;
+        for(byte i = 0; i < arr.length; i++) {
+            summa += arr[i];
+        }
+        return summa;
+    }
+
+}
