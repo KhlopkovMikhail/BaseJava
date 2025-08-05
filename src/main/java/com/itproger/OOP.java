1da8d0c01045a7f008fbb9d5926be9789ef96d3c
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-08-05 23:19:45 +0300

    added constructor to OOPTransport

--- src/main/java/com/itproger/OOP.java
+++ src/main/java/com/itproger/OOP.java
@@ -5,23 +5,24 @@ public class OOP {
 
     public static void main(String[] args) {
 
-        OOPTransport bmw = new OOPTransport(); // Создание объекта используя рвнее созданный класс OOPTransport
-        bmw.setValues(250.5f, 2500, "White", new byte[] {0, 0, 0});
-        String res1 = bmw.getValues();
-        System.out.println(res1);
+        OOPTransport bmw = new OOPTransport(250.5f, 2500, "White", new byte[] {0, 0, 0}); // Создание объекта используя рвнее созданный класс OOPTransport
+//        bmw.setValues(250.5f, 2500, "White", new byte[] {0, 0, 0});
+//        String res1 = bmw.getValues();
+//        System.out.println(res1);
 //        bmw.speed = 250.5f; // Обращение к полю объекта speed и установка значения данного поля объекта
 //        bmw.weight = 2500;
 //        bmw.color = "White";
 //        bmw.coordinate = new byte[] {0, 0, 0};
 
-        OOPTransport truck = new OOPTransport();
-        truck.speed = 140.5f;
-        truck.weight = 5600;
-        truck.color = "Red";
-        truck.coordinate = new byte[] {100, 0, 100};
-
-        String res2 = truck.getValues();
-        System.out.println(res2);
+        OOPTransport truck = new OOPTransport(5600, new byte[] {100, 0, 100});
+//        OOPTransport truck = new OOPTransport();
+//        truck.speed = 140.5f;
+//        truck.weight = 5600;
+//        truck.color = "Red";
+//        truck.coordinate = new byte[] {100, 0, 100};
+//
+//        String res2 = truck.getValues();
+//        System.out.println(res2);
 //        System.out.println("Truck speed: " + truck.speed + ". BMW speed: " + bmw.speed + "." );
 
     }
--- src/main/java/com/itproger/OOPTransport.java
+++ src/main/java/com/itproger/OOPTransport.java
@@ -2,22 +2,42 @@ package com.itproger;
 
 public class OOPTransport { // Создание класса
 
-    public float speed; // Созданно поле скорость
-    public int weight; // Созданно поле вес
-    public String color; // Созданно поле цвет
-    public byte[] coordinate; // Созданно поле с массивом координат
+//    public float speed; // Созданно поле скорость
+//    public int weight; // Созданно поле вес
+//    public String color; // Созданно поле цвет
+//    public byte[] coordinate; // Созданно поле с массивом координат
 
-    public void setValues(float _speed, int _weight, String _color, byte[] _coordinate) {
-        speed = _speed;
-        weight = _weight;
-        color = _color;
-        coordinate = _coordinate;
+    // private используется исходя из принципов инкапсуляции, чтобы доступ к параметрам был только через методы или конструкторы
+    private float speed; // Созданно поле скорость
+    private int weight; // Созданно поле вес
+    private String color; // Созданно поле цвет
+    private byte[] coordinate; // Созданно поле с массивом координат
+
+    public OOPTransport(float _speed, int _weight, String _color, byte[] _coordinate) { // Создание конструктора
+        // Join to DB (Подключение к базе данных
+        System.out.println("Object created");
+        setValues(_speed, _weight, _color, _coordinate);
+        System.out.println(getValues());
+    }
+
+    public OOPTransport(int weight, byte[] coordinate) {
+        this.weight = weight;
+        this.coordinate = coordinate;
+        System.out.println(this.getValues());
+    }
+
+
+    public void setValues(float speed, int weight, String color, byte[] coordinate) {
+        this.speed = speed; //this - обращение к полям класса
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
     }
 
     public String getValues() {
-        String info = "Object speed: " + speed + ". Weight: " + weight + ". Color: " + color + ".\n";
+        String info = "Object speed: " + this.speed + ". Weight: " + this.weight + ". Color: " + this.color + ".\n";
         String infoCoordinates = "Coordinates:\n";
-        for(int i = 0; i < coordinate.length; i++) {
+        for (int i = 0; i < coordinate.length; i++) {
             infoCoordinates += coordinate[i] + "\n";
         }
         return info + infoCoordinates;
